51f7b16714ceec5f3485d041efedfff428f49cb3
poype <253186926@qq.com>
2023-05-06 18:46:33 +0800

    解构赋值

--- second.js
+++ second.js
@@ -15,3 +15,50 @@ function test_func(param) {
 test_func(1)
 
 
+console.log("--------------解构赋值--------------");
+
+let [x, y] = [100, 200]
+console.log(x);  // 100
+console.log(y);  // 200
+
+[x, y] = [x+1, y+1]
+console.log(x); // 101
+console.log(y); // 201
+
+// 解构赋值左侧变量的个数不一定与右侧数组中元素的个数相同
+// 左侧多余的变量会被设置为undefined,而右侧多余的值会被忽略
+
+// 在最后一个变量上加“...”，把所有未使用或剩余的值收集到一个变量中
+let [i, ...j] = [1, 2, 3, 4]
+console.log(i);  // 1
+console.log(j);  // [ 2, 3, 4 ]
+
+// 嵌套
+let [a, [b, c]] = [1, [2, 2.5], 3]
+console.log(a);  // 1
+console.log(b);  // 2
+console.log(c);  // 2.5
+
+// 解构赋值的右侧不是必须是数组，可以是任何可迭代的对象
+let [l, m, ...n] = "hello"
+console.log(l);  // h
+console.log(m);  // e
+console.log(n);  // [ 'l', 'l', 'o' ]
+
+// 对象形式的解构赋值， 重点
+let obj = {
+    o: 88,
+    p: 89,
+    q: 90,
+    t: 91
+}
+let {o, p, q} = obj
+console.log(o);  // 88
+console.log(p);  // 89
+console.log(q);  // 90
+
+// Math中还有好多其它属性，但那些没有提到名字的属性都被忽略了
+const {sin, cos, tan} = Math;
+console.log(sin);  // [Function: sin]
+console.log(cos);  // [Function: cos]
+console.log(tan);  // [Function: tan]
