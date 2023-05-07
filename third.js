954dbd9e2c0365513d48b5cddf573eda7f9cd142
poype <253186926@qq.com>
2023-05-07 11:38:37 +0800

    instanceof 操作符

--- third.js
+++ third.js
@@ -19,3 +19,15 @@ let array = ["a", "b", "c"]
 console.log(0 in array);  // true
 console.log(2 in array);  // true
 console.log(3 in array);  // false
+
+
+
+// instanceof操作符
+console.log(obj instanceof Object);    // true
+console.log(obj instanceof Number);    // false
+console.log(array instanceof Array);   // true
+console.log(array instanceof Object);  // true
+console.log(array instanceof Number);  // false
+
+// 为了对表达式 o instanceof f 求值，JS会求值f.prototype，然后在 o 的原型链上查看是否有 f.prototype 这个值
+// 如果找到了就返回true。如果 f.prototype 不是 o 原型链上的一个值，则o不是f的实例。
