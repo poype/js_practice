a5fa8c35ea6662687f13fa829544320574cf8359
poype <253186926@qq.com>
2023-05-07 21:31:00 +0800

    Object.assign()函数复制对象的属性

--- fifth.js
+++ fifth.js
@@ -17,3 +17,24 @@ console.log(o3.toString);  // [Function: toString]
 
 // 针对对象o的任何修改操作，都只能影响o变量本身的值，而不会对o的原型链有任何影响
 // 所以delete操作符只能删除自有属性，不能删除继承属性
+
+
+
+// Object.assign()函数复制对象的属性
+let o4 = {
+    x: 1, 
+    y: 2, 
+    z: 3
+}
+
+let o5 = {
+    z: 4, 
+    s: 5,
+    t: 6
+}
+
+let o6 = {}
+
+Object.assign(o6, o4, o5)  // 把o4 和 o5对象中的属性都复制到o6中
+console.log(o6);  // { x: 1, y: 2, z: 4, s: 5, t: 6 }
+// o4 和 o5中都包含属性z，由于o5排在o4的后面，所以o6中z参数的值最终会与o5的一样
