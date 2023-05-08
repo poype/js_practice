1273612f5522205e2b405e4e67216c5f740d62c5
poype <253186926@qq.com>
2023-05-08 08:23:21 +0800

    1. 函数作为参数穿给另一个函数 2. 箭头函数

--- sixth.js
+++ sixth.js
@@ -6,3 +6,25 @@ console.log(b)  // [ 4, 5, 1, 2, 3 ]
 
 let c = [..."Hello"]
 console.log(c);  // [ 'H', 'e', 'l', 'l', 'o' ]
+
+
+
+function testFunc(func) {
+    func("aaa", "bbb", "ccc")
+}
+
+// 函数作为参数穿给另一个函数
+testFunc(function(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+})
+
+console.log("-----------------------------------");
+
+// 箭头函数
+testFunc((a, b, c) => {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+})
