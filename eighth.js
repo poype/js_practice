d81a73f5dff1b3f0a260862f724796c0d875a5ac
poype <253186926@qq.com>
2023-05-12 09:21:51 +0800

    class

--- eighth.js
+++ eighth.js
@@ -15,3 +15,53 @@ let o2 = {
     }
 }
 o2.say()
+
+
+// class只是ES6新增的语法糖，本质还是构造函数和原型链
+class Animal {
+
+    constructor(name) {
+        this.name = name;   
+    }
+
+    run() {
+        console.log(`Animal ${this.name} is running`);
+    }
+    
+    eat() {
+        console.log(`Animal ${this.name} is eating`);
+    }
+}
+
+let animal = new Animal("Rabbit");
+animal.run();
+animal.eat();
+// 运行结果：
+// Animal Rabbit is running
+// Animal Rabbit is eating
+
+
+
+// extends 和 super
+class Dog extends Animal {
+    
+    constructor(name, color) {
+        super(name);
+        this.color = color;
+    }
+
+    run() {
+        console.log(`${this.color} Dog ${this.name} is running`)
+    }
+}
+let dog1 = new Dog("Joge", "Yellow");
+let dog2 = new Dog("Wangcai", "Black");
+dog1.run()
+dog1.eat()
+dog2.run()
+dog2.eat()
+// 运行结果：
+// Yellow Dog Joge is running
+// Animal Joge is eating
+// Black Dog Wangcai is running
+// Animal Wangcai is eating
