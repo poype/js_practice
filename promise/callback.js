f8235dab46639900c1aeb67c9539abb34ffc582a
poype <253186926@qq.com>
2023-05-14 20:59:26 +0800

    使用回调函数的异步处理方式

--- promise/callback.js
+++ promise/callback.js
@@ -1,16 +1,16 @@
 const fs = require("fs")
 
 // 使用回调函数的异步处理方式
-function readFile(fileName) {
-    fs.readFile(fileName, "utf-8", (err, text) =>  {
-        if(err) {
-            console.log("Can not read file")
-            console.log(err);
-        } else {
-            console.log(text)
-        }
-    })
+function readFile(fileName, callback) {
+    fs.readFile(fileName, "utf-8", callback)
 }
 
-readFile("../first.js")
+readFile("../first.js", (err, text) =>  {
+    if(err) {
+        console.log("Can not read file")
+        console.log(err);
+    } else {
+        console.log(text)
+    }
+})
 
